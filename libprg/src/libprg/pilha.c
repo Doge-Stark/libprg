f3c0a58604e86fa0e2d18eeffbff5eba3cd30799
Pedro Inácio Pacheco <pedro.i09@aluno.ifsc.edu.br>
2026-03-24 17:27:10 -0300

    implementadas as funções desempilhar, empilhar, tamanho, vazia, destruir pilha

--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -4,6 +4,7 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 
 
 typedef struct pilha {
@@ -24,7 +25,40 @@ pilha_t* criar_pilha(int capacidade) {
     return pilha;
 }
 
-// empilhar
-// desempilhar
-// tamanho
-// vazia
+int empilhar(pilha_t* pilha, int valor) {
+
+    if (pilha->topo >= pilha->tamanho) {
+        pilha->tamanho  *= 2;
+        pilha->elementos = realloc(pilha->elementos, sizeof(int) * pilha->tamanho);
+    } else
+        pilha->topo++;
+        pilha->elementos[pilha->topo] = valor;
+
+    return 0;
+}
+
+int desempilhar(pilha_t* pilha) {
+
+    if (pilha->topo < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    int valor = pilha->elementos[pilha->topo];
+    pilha->topo--;
+    return valor;
+
+}
+int tamanho(pilha_t* pilha) {
+    int N = pilha->tamanho;
+    return N;
+}
+
+int vazia(pilha_t* pilha) {return pilha->topo < 0;}
+
+int destruir_pilha(pilha_t* pilha) {
+
+    free(pilha->elementos);
+    free(pilha);
+    return 0;
+
+}
